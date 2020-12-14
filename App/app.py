520b62216a32b248ee453e6c3cc33439d346402d
theroadfreak <dushancimbaljevic@gmail.com>
2020-12-14 11:09:22 +0100

    setup

--- App/app.py
+++ App/app.py
@@ -1,9 +1,11 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
+url = "https://www.neptun.mk/pocetna/categories/TV_AUDIO_VIDEO/DOMASNI_KINA/SOUNDBAR.nspx"
 
-
-
-
-
-
+# opening
+client = uReq(url)
+pageHtml = client.read()
+pageSoup = soup(pageHtml, "html.parser")
+client.close()
+print(pageSoup.form)
