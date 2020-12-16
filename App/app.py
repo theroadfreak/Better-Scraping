ecb41080484b24acb4000aad20b2208619d6d08b
theroadfreak <dushancimbaljevic@gmail.com>
2020-12-16 18:42:36 +0100

    code generator

--- App/app.py
+++ App/app.py
@@ -6,6 +6,24 @@ url = "https://www.neptun.mk/pocetna/categories/TV_AUDIO_VIDEO/DOMASNI_KINA/SOUN
 # opening
 client = uReq(url)
 pageHtml = client.read()
-pageSoup = soup(pageHtml, "html.parser")
 client.close()
-print(pageSoup.form)
+pageSoup = soup(pageHtml, "html.parser")
+
+pageSoup = pageSoup.html
+pageSoup = pageSoup.body
+pageSoup = pageSoup.find('form', {'id': 'aspnetForm'})
+pageSoup = pageSoup.find('div', {'id': 'ns-wrapper'})
+pageSoup = pageSoup.find('div', {'id': 'ns-content'})
+pageSoup = pageSoup.find('div', {'class': 'full-content catPage'})
+pageSoup = pageSoup.find('div', {'class': 'container'})
+pageSoup = pageSoup.find('div', {'class': 'row'})
+# pageSoup = pageSoup.find('div', {'class': 'inner-content-box inner'})
+# pageSoup = pageSoup.div
+# pageSoup = pageSoup.find('div', {'id': 'angularApp'})
+# pageSoup = pageSoup.find('div', {'class': 'ng-scope'})
+# pageSoup = pageSoup.find('div', {'id': 'mainContainer'})
+# pageSoup = pageSoup.find('div', {'class': 'row'})
+# pageSoup = pageSoup.find(
+#     'div', {'class': 'col-lg-9 col-md-9 col-sm-8 col-fix-main'})
+
+print(pageSoup)
